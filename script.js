bea7e794bde90d4d44b726a3885f25fff625e2bd
rajshekhar26 <rjshekhar.official@gmail.com>
2021-10-06 13:04:05 +0530

    Refactor

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const body = document.body
-const getBodyTheme = localStorage.getItem('portfolio-theme')
-const getBtnTheme = localStorage.getItem('portfolio-btn-theme')
+
 const btnTheme = document.querySelector('.fa-moon')
 const btnHamburger = document.querySelector('.fa-bars')
 
+const addThemeClass = (bodyClass, btnClass) => {
+  body.classList.add(bodyClass)
+  btnTheme.classList.add(btnClass)
+}
+
+const getBodyTheme = localStorage.getItem('portfolio-theme')
+const getBtnTheme = localStorage.getItem('portfolio-btn-theme')
+
+addThemeClass(getBodyTheme, getBtnTheme)
+
 const isDark = () => body.classList.contains('dark')
 
-const setTheme = (bodyClass, buttonClass) => {
+const setTheme = (bodyClass, btnClass) => {
+
 	body.classList.remove(localStorage.getItem('portfolio-theme'))
 	btnTheme.classList.remove(localStorage.getItem('portfolio-btn-theme'))
-	body.classList.add(bodyClass)
-	btnTheme.classList.add(buttonClass)
+
+  addThemeClass(bodyClass, btnClass)
+
 	localStorage.setItem('portfolio-theme', bodyClass)
-	localStorage.setItem('portfolio-btn-theme', buttonClass)
+	localStorage.setItem('portfolio-btn-theme', btnClass)
 }
 
 const toggleTheme = () =>
 	isDark() ? setTheme('light', 'fa-moon') : setTheme('dark', 'fa-sun')
+
+btnTheme.addEventListener('click', toggleTheme)
 
 const displayList = () => {
 	const navUl = document.querySelector('.nav__list')
@@ -32,24 +45,19 @@ const displayList = () => {
 	}
 }
 
+btnHamburger.addEventListener('click', displayList)
+
 const scrollUp = () => {
-	const btnScrollTop = document.querySelector('.scroll-top');
+	const btnScrollTop = document.querySelector('.scroll-top')
+
 	if (
 		body.scrollTop > 500 ||
 		document.documentElement.scrollTop > 500
 	) {
-		btnScrollTop.style.display = 'block';
+		btnScrollTop.style.display = 'block'
 	} else {
-		btnScrollTop.style.display = 'none';
+		btnScrollTop.style.display = 'none'
 	}
-};
+}
 
-btnTheme.addEventListener('click', toggleTheme)
-btnHamburger.addEventListener('click', displayList)
 document.addEventListener('scroll', scrollUp)
-
-const setBodyTheme = () => body.classList.add(getBodyTheme)
-const setBtnTheme = () => btnTheme.classList.add(getBtnTheme)
-
-setBodyTheme()
-setBtnTheme()
